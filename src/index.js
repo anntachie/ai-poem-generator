cff6cd0b6ef11c0562466fefc8bbdf2b34f95cb9
Ann <anntachie@gmail.com>
2024-08-14 19:45:41 -0800

    updated the javascript file

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function generatePoem(event) {
+    event.preventDefault();
+
+
+    new Typewriter('#poem', {
+        strings: "poem",
+        autoStart: true,
+        delay:1,
+        cursor: "",
+      });
+
+    
+}
+
+
+let poemFormElement= document.querySelector("#poem-generator")
+poemFormElement.addEventListener("submit", generatePoem);
